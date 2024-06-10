7d7e9fc41ec5fff6d55ed62523a199454e2153f8
Renly H <renly.hou@gmail.com>
2024-06-10 13:42:03 -0400

    config

--- ts2/train/sbatch/renly/submit_contrast.sh
+++ ts2/train/sbatch/renly/submit_contrast.sh
@@ -2,41 +2,40 @@
 
 #SBATCH --job-name=exp
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
 #SBATCH --output=slurm_out/%A_%a.out
 
-#SBATCH --account=bleu99
-#SBATCH --time=2-00:00:00
-
+# ---------------------------------------------------
 # SBATCH --account=precisionhealth_owned1
 # SBATCH --partition=precisionhealth
 # SBATCH --cpus-per-task=5
 # SBATCH --mem-per-cpu=19200m
-# SBATCH --ntasks-per-node=1
-# SBATCH --gres=gpu:1
+# ----------------------------------------------------
 
-#SBATCH --partition=spgpu
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=4800m
+# ----------------------------------------------------
+# SBATCH --partition=spgpu
+# SBATCH --account=bleu99
+# SBATCH --cpus-per-task=4
+# SBATCH --mem-per-cpu=4800m
+# ----------------------------------------------------
+#SBATCH --partition=tocho
+#SBATCH --account=tocho_owned1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=7739m
+# ---------------------------------------------------
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks-per-node=2
 #SBATCH --gpu_cmode=shared
-
+#SBATCH --time=2-00:00:00
+# ---------------------------------------------------
 # SBATCH --nodelist=armis26001
+# ---------------------------------------------------
+#SBATCH --array=0-1
 
-<<<<<<< Updated upstream
-#SBATCH --array=0-2
-source activate ts2
 
-#srun python main.py -c config/renly/train_simclr_srh.yaml
-#python main.py -c config/renly/train_ijepa_tcga.yaml
-srun python main.py -c config/renly/train_ijepa_srh.yaml
-=======
-#SBATCH --array=3-3
 source activate ts2
 
 #srun python main.py -c config/renly/train_simclr_srh.yaml
 #srun python main.py -c config/renly/train_dino_tcga.yaml
-python main.py -c config/renly/train_ijepa_tcga.yaml
->>>>>>> Stashed changes
+srun python main.py -c config/renly/train_ijepa_tcga.yaml
 #python main.py -c config/renly/train_pjepa_tcga.yaml
