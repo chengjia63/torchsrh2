a62247eb8825507cdcf571ad7d6afc7c1cb08b49
Cheng Jiang <chengjia@umich.edu>
2026-03-20 13:55:46 -0400

    Fixed nio number column issue in cell inference.
    Still allow using pt as ID if nio num is not included

--- ts2/train/main_cell_inference.py
+++ ts2/train/main_cell_inference.py
@@ -24,8 +24,7 @@ from ts2.data.transforms import HistologyTransform
 import yaml
 
 import pytorch_lightning as pl
-from ts2.train.infra import (parse_args, read_process_cf, setup_infra_testing,
-                             get_rank)
+from ts2.train.infra import parse_args, read_process_cf, setup_infra_testing, get_rank
 from ts2.data.slide_dataset import SRHPatchCoordMapper
 from ts2.lm.dinov2_eval_system import Dinov2EvalSystem
 
@@ -39,9 +38,11 @@ class SingleCellTempInferenceDataset(torch.utils.data.Dataset):
         slide_instances: str,
         transform: Callable,
         tile_size=48,
-        process_read_im: Callable = MemmapTileReader(which_set="srh")):
+        process_read_im: Callable = MemmapTileReader(which_set="srh"),
+    ):
 
         super().__init__()
+        raise RuntimeError("SingleCellTempInferenceDataset is deprecated.")
 
         default_seg_model = "03207B00"
         self.data_root_ = data_root
@@ -57,31 +58,32 @@ class SingleCellTempInferenceDataset(torch.utils.data.Dataset):
         for i, r in tqdm(slide_instances_df.iterrows()):
 
             # Getting slide / patch metadata
-            pt_meta_fname = opj(data_root, r["institution"], r["patient"],
-                                f"{r['patient']}_meta.json")
+            pt_meta_fname = opj(
+                data_root, r["institution"], r["patient"], f"{r['patient']}_meta.json"
+            )
 
             if not os.path.exists(pt_meta_fname):
                 logging.warning(f"No pt meta file - {pt_meta_fname}")
                 continue
 
             with open(pt_meta_fname) as fd:
-                slide_meta_all_inf = json.load(fd)["slides"][
-                    r["mosaic"]]["predictions"]
-                
+                slide_meta_all_inf = json.load(fd)["slides"][r["mosaic"]]["predictions"]
 
             if default_seg_model in slide_meta_all_inf:
                 slide_meta = slide_meta_all_inf[default_seg_model]
             else:
                 logging.warning(f"No {default_seg_model} inference - {pt_meta_fname}")
                 continue
-                
 
-            all_patches = [(k, v, tt)
-                           for tt, subdict in slide_meta["patches"].items()
-                           for k,v in subdict.items()]
+            all_patches = [
+                (k, v, tt)
+                for tt, subdict in slide_meta["patches"].items()
+                for k, v in subdict.items()
+            ]
 
-            all_patches = pd.DataFrame(all_patches,
-                                       columns=["patch_flat", "mmap_idx", "patch_type"])
+            all_patches = pd.DataFrame(
+                all_patches, columns=["patch_flat", "mmap_idx", "patch_type"]
+            )
             all_patches.loc[:, "tensor_shape"] = [
                 tuple(slide_meta["tensor_shape"])
             ] * len(all_patches)
@@ -103,41 +105,41 @@ class SingleCellTempInferenceDataset(torch.utils.data.Dataset):
             cp["centroid_c"] = cp["centroid_c"].round().astype(int)
 
             # filter out undesiable cells
-            cp_filt = cp[cp["celltype"].isin({"nuclei", "mp"})
-                         & (cp["score"] > 0.5) &
-                         (self.tile_size_ / 2 <= cp["centroid_r"]) &
-                         (cp["centroid_r"] <= 300 - self.tile_size_ / 2) &
-                         (self.tile_size_ / 2 <= cp["centroid_c"]) &
-                         (cp["centroid_c"] <= 300 - self.tile_size_ / 2)]
+            cp_filt = cp[
+                cp["celltype"].isin({"nuclei", "mp"})
+                & (cp["score"] > 0.5)
+                & (self.tile_size_ / 2 <= cp["centroid_r"])
+                & (cp["centroid_r"] <= 300 - self.tile_size_ / 2)
+                & (self.tile_size_ / 2 <= cp["centroid_c"])
+                & (cp["centroid_c"] <= 300 - self.tile_size_ / 2)
+            ]
 
             if len(cp_filt) == 0:
                 logging.warning(f"no cells - {s}")
                 continue
 
             # putting instances together
-            cp_filt = pd.DataFrame({
-                "patch":
-                cp_filt["patch"],
-                "proposal":
-                zip(cp_filt["centroid_r"], cp_filt["centroid_c"])
-            })
+            cp_filt = pd.DataFrame(
+                {
+                    "patch": cp_filt["patch"],
+                    "proposal": zip(cp_filt["centroid_r"], cp_filt["centroid_c"]),
+                }
+            )
             cp_filt.loc[:, "patch_flat"] = cp_filt["patch"].apply(
-                SRHPatchCoordMapper.to_universal_patch_name)
+                SRHPatchCoordMapper.to_universal_patch_name
+            )
             cp_filt.loc[:, "institution"] = r["institution"]
             cp_filt.loc[:, "patient"] = r["patient"]
             cp_filt.loc[:, "mosaic"] = r["mosaic"]
 
-            cp_filt = pd.merge(cp_filt,
-                               all_patches,
-                               on="patch_flat",
-                               how="left")
+            cp_filt = pd.merge(cp_filt, all_patches, on="patch_flat", how="left")
 
-            #if len(cp_filt) > 800:
+            # if len(cp_filt) > 800:
             #    cp_filt = cp_filt.sample(800, random_state=1000)
             instances.append(cp_filt)
 
         self.instances_ = pd.concat(instances).reset_index(drop=True)
-        import pdb; pdb.set_trace()
+
         logging.info(f"Num instances {len(self.instances_)}")
         logging.info(self.instances_[["patient", "mosaic"]].value_counts())
 
@@ -147,15 +149,23 @@ class SingleCellTempInferenceDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx: int):
         """Retrieve a list of patches, from the wholeslide specified by idx"""
         inst = self.instances_.iloc[idx]
-        rc_idx = (inst["proposal"][0] - self.tile_size_ // 2,
-                  inst["proposal"][1] - self.tile_size_ // 2)
+        rc_idx = (
+            inst["proposal"][0] - self.tile_size_ // 2,
+            inst["proposal"][1] - self.tile_size_ // 2,
+        )
 
-        mmap_path = opj(self.data_root_, inst["institution"], inst["patient"],
-                        inst["mosaic"], "patches",
-                        f"{inst['patient']}-{inst['mosaic']}-patches.dat")
+        mmap_path = opj(
+            self.data_root_,
+            inst["institution"],
+            inst["patient"],
+            inst["mosaic"],
+            "patches",
+            f"{inst['patient']}-{inst['mosaic']}-patches.dat",
+        )
 
-        im = self.process_read_im_(mmap_path, inst["tensor_shape"],
-                                   [inst["mmap_idx"]], rc_idx, self.tile_size_)
+        im = self.process_read_im_(
+            mmap_path, inst["tensor_shape"], [inst["mmap_idx"]], rc_idx, self.tile_size_
+        )
 
         out = inst.to_dict()
         out["image"] = self.transform_(im.squeeze()).unsqueeze(0)
@@ -164,16 +174,18 @@ class SingleCellTempInferenceDataset(torch.utils.data.Dataset):
         out["label"] = inst["patch_type"]
         return out
 
+
 def parse_tuple_string(s: str) -> tuple[int, ...]:
     s = s.strip()
-    if not (s.startswith('(') and s.endswith(')')):
+    if not (s.startswith("(") and s.endswith(")")):
         raise ValueError("Input must be a string representation of a tuple")
-    
+
     content = s[1:-1].strip()
     if not content:
         return ()
 
-    return tuple(int(item.strip()) for item in content.split(','))
+    return tuple(int(item.strip()) for item in content.split(","))
+
 
 class SingleCellListInferenceDataset(torch.utils.data.Dataset):
 
@@ -183,7 +195,8 @@ class SingleCellListInferenceDataset(torch.utils.data.Dataset):
         cell_instances: str,
         transform: Callable,
         tile_size=48,
-        process_read_im: Callable = MemmapTileReader(which_set="srh")):
+        process_read_im: Callable = MemmapTileReader(which_set="srh"),
+    ):
 
         super().__init__()
 
@@ -195,12 +208,22 @@ class SingleCellListInferenceDataset(torch.utils.data.Dataset):
         self.process_read_im_ = process_read_im
         self.tile_size_ = tile_size
 
-        
         insts = pd.read_csv(cell_instances, dtype=str)
         insts["proposal"] = insts["proposal"].apply(parse_tuple_string)
         insts["tensor_shape"] = insts["tensor_shape"].apply(parse_tuple_string)
         insts["mmap_idx"] = pd.to_numeric(insts["mmap_idx"]).astype(int)
         self.instances_ = insts
+
+        if "nion" in insts.columns:
+            self.nion_col = "nion"
+        else:
+            self.nion_col = "patient"
+            logging.warning(
+                (
+                    "using patient in path is not safe.",
+                    "you should include a nio number column in your dataset csv.",
+                )
+            )
 
         logging.info(f"Num instances {len(self.instances_)}")
         logging.info(self.instances_[["patient", "mosaic"]].value_counts())
@@ -211,15 +234,23 @@ class SingleCellListInferenceDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx: int):
         """Retrieve a list of patches, from the wholeslide specified by idx"""
         inst = self.instances_.iloc[idx]
-        rc_idx = (inst["proposal"][0] - self.tile_size_ // 2,
-                  inst["proposal"][1] - self.tile_size_ // 2)
+        rc_idx = (
+            inst["proposal"][0] - self.tile_size_ // 2,
+            inst["proposal"][1] - self.tile_size_ // 2,
+        )
 
-        mmap_path = opj(self.data_root_, inst["institution"], inst["patient"],
-                        inst["mosaic"], "patches",
-                        f"{inst['patient']}-{inst['mosaic']}-patches.dat")
+        mmap_path = opj(
+            self.data_root_,
+            inst["institution"],
+            inst[self.nion_col],
+            inst["mosaic"],
+            "patches",
+            f"{inst[self.nion_col]}-{inst['mosaic']}-patches.dat",
+        )
 
-        im = self.process_read_im_(mmap_path, inst["tensor_shape"],
-                                   [inst["mmap_idx"]], rc_idx, self.tile_size_)
+        im = self.process_read_im_(
+            mmap_path, inst["tensor_shape"], [inst["mmap_idx"]], rc_idx, self.tile_size_
+        )
 
         out = inst.to_dict()
         out["image"] = self.transform_(im.squeeze()).unsqueeze(0)
@@ -227,7 +258,7 @@ class SingleCellListInferenceDataset(torch.utils.data.Dataset):
         out["path"] = f"{out['patch']}#{out['proposal'][0]}_{out['proposal'][1]}"
         out["label"] = inst["patch_type"]
         return out
-    
+
 
 from collections import defaultdict
 
@@ -246,24 +277,26 @@ def main():
     logging.info("Doing inference")
 
     eval_root, pred_dir, results_dir, pred_fname = setup_infra_testing(
-        cf, embedded_exp_root=None)
+        cf, embedded_exp_root=None
+    )
 
     con_exp = Dinov2EvalSystem(**cf.lightning_module.params)
 
-    dsets = {"SingleCellListInferenceDataset": SingleCellListInferenceDataset,
-             "SingleCellTempInferenceDataset": SingleCellTempInferenceDataset}
+    dsets = {
+        "SingleCellListInferenceDataset": SingleCellListInferenceDataset,
+        "SingleCellTempInferenceDataset": SingleCellTempInferenceDataset,
+    }
     dataset = dsets[cf.data.test_dataset.which](
         transform=HistologyTransform(**cf.data.xform_params),
-        **cf.data.test_dataset.params)
+        **cf.data.test_dataset.params,
+    )
 
-
-    data_loader = DataLoader(dataset, **cf.data.loader.params.test),
+    data_loader = (DataLoader(dataset, **cf.data.loader.params.test),)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    inf_trainer = pl.Trainer(accelerator=device,
-                             devices=1,
-                             default_root_dir=eval_root,
-                             inference_mode=True)  # deterministic=True)
+    inf_trainer = pl.Trainer(
+        accelerator=device, devices=1, default_root_dir=eval_root, inference_mode=True
+    )  # deterministic=True)
 
     pred_raw = inf_trainer.predict(con_exp, dataloaders=data_loader)
 
