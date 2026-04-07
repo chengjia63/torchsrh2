13dd5b0b42bbba90785d30f925bc8d5ad08e8727
Cheng Jiang <chengjia@umich.edu>
2026-04-06 20:33:47 -0400

    running eval with off-shelf dinov2 model

---
+++ ts2/train/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+#for i in {0..1}; do
+#  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrhdb.yaml
+#done
+
+#for i in {0..10}; do
+#  #SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrh_test_perturbed.yaml
+#  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2aw_scsrh_test_perturbed.yaml
+#done
+
+for i in {0..21}; do
+  #SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrh_paired.yaml
+  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2aw_scsrh_paired.yaml
+done
