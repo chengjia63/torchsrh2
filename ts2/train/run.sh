abeb43950789bb9eb1e8213e4f00d56874519093
Cheng Jiang <chengjia@umich.edu>
2026-04-09 17:04:55 -0400

    run inference on new exps

--- ts2/train/run.sh
+++ ts2/train/run.sh
@@ -4,16 +4,16 @@ set -x
 set -e
 
 for i in {0..0}; do
-  #SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrhdb.yaml
-  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2aw_scsrhdb.yaml
+  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrhdb.yaml
+  #SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2aw_scsrhdb.yaml
 done
 
-#for i in {0..10}; do
-#  #SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrh_test_perturbed.yaml
+for i in {0..10}; do
+    SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrh_test_perturbed.yaml
 #  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2aw_scsrh_test_perturbed.yaml
-#done
+done
 
-#for i in {0..21}; do
-#  #SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrh_paired.yaml
+for i in {0..21}; do
+   SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2_scsrh_paired.yaml
 #  SLURM_ARRAY_TASK_ID="$i" python main_cell_inference.py -c=config/chengjia/inference_dinov2aw_scsrh_paired.yaml
-#done
+done
