3da25d92d535444983c841ac4474acf2a72de02c
Cheng Jiang <chengjia@umich.edu>
2026-04-24 14:41:02 -0400

    repo reorg, may break some depencency linking

---
+++ ts2/utils/silica_eval/site/run.sh
@@ -0,0 +1,6 @@
+python app.py \
+    --slide-portal-dir ../infil/site_res/debug_out \
+    --slide-dzi-dir ../infil/site_res/dzi \
+    --ground-truth-csv ../infil/site_res/gt.csv \
+    --default-experiment b1a0cbe3_k1024 \
+    --port 8765
--- ts2/utils/silica_site/run.sh
+++
@@ -1,6 +0,0 @@
-python app.py \
-    --slide-portal-dir ../silica_sc_eval/site_res/debug_out \
-    --slide-dzi-dir ../silica_sc_eval/site_res/dzi \
-    --ground-truth-csv ../silica_sc_eval/site_res/gt.csv \
-    --default-experiment b1a0cbe3_k1024 \
-    --port 8765
